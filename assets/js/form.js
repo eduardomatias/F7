1b6c1b2efa34538febbe9443ebbf768f23711b92
Eduardo Matias <eduardomatias.1989@gmail.com>
2017-12-18 16:02:16 -0200

    Update form.js
    Format number BR

--- assets/js/form.js
+++ assets/js/form.js
@@ -216,6 +216,13 @@ var Util = {
             j = (j = i.length) > 3 ? j % 3 : 0;
         return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
     },
+    formatNumberBR: function (n, c) {
+        var n,
+            c = isNaN(c = Math.abs(c)) ? 2 : c;
+        n = n.replace('.', '');
+        nFormatado = parseFloat(n.replace(',', '.'));
+        return parseFloat(nFormatado.toFixed(2));
+    },
     getEnderecoByCEP: function (cep, callback) {
         cep = cep.replace('-', '');
         var ajax = $.ajax({
